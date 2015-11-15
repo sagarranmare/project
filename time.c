b6ab7262961e5b885a45ce4ca26b0dbf06f45ade
sagarranmare <ranmaresagar@yahoo.in>
2015-11-15 19:19:59 +0530

    Update time.c

--- time.c
+++ time.c
@@ -1,16 +1,49 @@
+ /*****************************************************************************
+ * Copyright (C) Sagar S. Ranmare  ranmaresagar@yahoo.in 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include<stdio.h>
 void seats(int,char);
 void time(int a,char z){
-	int i,q;
+	int i,q,pp;
 	char x=z;
-	printf("please select time\n");
-	printf("[1]11:00 am\n[2]5:00 pm\n[3]8:00pm\n);
+	for(pp=0;pp<36;pp++){
+	printf("____");
+	}
+	printf("\n\n\n\t\t\t\tplease select time\n\n");
+	printf("\t\t\t\t\t\t[1]11:00 am\n\n\t\t\t\t\t\t[2]5:00 pm\n\n\t\t\t\t\t\t[3]8:00pm\n");
+	printf("\n\n\n\n\n\n");	
+	for(pp=0;pp<36;pp++){
+	printf("____");
+	}	
 	scanf("%d",&i);
 	if(a==1){
-	q=i;	
+	q=i;
+	seats(q,x);	
 	}
-	if(a==2){
+	else if(a==2){
 	q=i+3;
-	}
 	seats(q,x);
+	}
+	else if (i==0){
+	date(a,z);
+	}
+	else{
+	printf("Wrong selection");
+	time(a,z);
+	}
 }
