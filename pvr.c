f7eeb72b535b6be981d7fd49067303f7e5b413c9
sagarranmare <ranmaresagar@yahoo.in>
2015-11-15 19:14:53 +0530

    Update pvr.c

--- pvr.c
+++ pvr.c
@@ -1,13 +1,38 @@
+ 
+/*****************************************************************************
+ * Copyright (C) Sagar S. Ranmare  ranmaresagar@yahoo.in 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
 #include<stdio.h>
 void date(int,char);
 void cinemas();
 void pvr() {
-	int i,a=1;
+	int i,a=1,pp;
 	char x;
-	printf(" \t\tNOW SHOWING\n");
-	printf("1.martian(hindi)\n");
-	printf("2.Martian(English)\n");
-	printf("press 0 for previous menu\n");
+	for(pp=0;pp<36;pp++){
+	printf("____");
+	}
+	printf("\n\n\n\t\t\t\tNOW SHOWING\n\n");
+	printf("\t\t\t\t\t\t1.martian(hindi)\n\n");
+	printf("\t\t\t\t\t\t2.Martian(English)\n\n");
+	printf("\t\t\t\t\t\tpress 0 for previous menu\n");
+	printf("\n\n\n\n\n\n");
+	for(pp=0;pp<36;pp++){
+	printf("____");
+	}
 	scanf("%d",&i);
 	if(i==1){
 	x='a';
@@ -15,15 +40,14 @@ void pvr() {
 	else if(i==2){
 	x='b';
 	}
-	else {
-	printf("wrong selection");
-	pvr();
-	}
 	if(i==1||i==2){
 		date(a,x);
 	}
 	else if(i==0){
 	cinemas();
+	}
+	else {
+	printf("wrong selection");
+	pvr();
 	}	
 }
-
