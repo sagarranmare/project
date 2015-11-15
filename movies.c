8ba6882fe5a05725c1d8f8d30fe167a7ca243d76
sagarranmare <ranmaresagar@yahoo.in>
2015-11-15 19:11:47 +0530

    Update movies.c

--- movies.c
+++ movies.c
@@ -1,16 +1,47 @@
+ /*****************************************************************************
+ * Copyright (C) Sagar S. Ranmare  ranmaresagar@yahoo.in 
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation; either version 2.1 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston MA 02110-1301, USA.
+ *****************************************************************************/
+
 #include<stdio.h>
-void cmp();
+void cmp(int);
 int main();
 void movies(){
- int i;
-	printf("\t\tAvailable shows are:\n");
-	printf("\t\t1.hfasjhfas\n\t\t2.safasfasf\n");
-	printf("\t\tpress 0 for previous menu\n");
-	scanf("%d",&i);
-	if(i==1||i==2){
-	 cmp();	
+int i,pp;
+int j;
+for(pp=0;pp<36;pp++){
+	printf("____");
 	}
-	if(i==0)
+	printf("\t\t\t\t\t\tAvailable shows are:\n\n");
+	printf("\t\t\t\t1.hfasjhfas\n\n\t\t\t\t2.safasfasf\n\n\t\t\t\t");
+	printf("\t\t\t\tpress 0 for previous menu\n");
+	printf("\n\n\n\n\n\n");	
+	for(pp=0;pp<36;pp++){
+	printf("____");
+	}	
+	scanf(" %d",&j);
+	i=j;	
+	if(i==1||i==2){
+	 cmp(i);	
+	}
+	else if(i==0){
 	main();
 }
-
+	else{
+	printf("wrong selection try again");
+	movies();
+	}
+}
